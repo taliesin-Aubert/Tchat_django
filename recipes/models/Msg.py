f755670acd083ac4e2e7933f13092c3955cd2718
taliesin-Aubert <73161306+taliesin-Aubert@users.noreply.github.com>
2023-02-12 18:27:38 +0000

    correction models

--- recipes/models/Msg.py
+++ recipes/models/Msg.py
@@ -2,3 +2,6 @@ from django.db import models
 
 class Msg(models.Model):
     message = models.Charfield(max_length = 300, null = True, blank = True, default = '')
+
+    def __str__(self):
+        return self.message
